ffb670fd7595fcdbf31f1c640665d831d360a28e
Md Touhiduzzaman <arjunpawza@gmail.com>
2025-12-15 12:16:44 +0600

    fix: Use ES module syntax in generate-sitemap.js

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,8 +1,11 @@
 
-const fs = require('fs');
-const path = require('path');
-const { create } = require('xmlbuilder2');
+import fs from 'fs';
+import path from 'path';
+import { create } from 'xmlbuilder2';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const APP_DIR = path.resolve(__dirname, '..');
 const DIST_DIR = path.resolve(APP_DIR, 'dist');
 const URL_FILE = path.resolve(APP_DIR, '..', '..', 'astro_sitemap_urls.txt');
