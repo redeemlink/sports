63b7acf2cb685e6f03f9a93e9005ea2da86c9266
Md Touhiduzzaman <arjunpawza@gmail.com>
2025-12-20 08:09:27 +0600

    chore: disable @astrojs/sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://redeemlink.xyz',
   base: '/sports',
-  integrations: [sitemap()],
+  integrations: [],
 });
